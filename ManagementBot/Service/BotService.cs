b4539e4c9eb9155a5d4938d174ebc6af7c4dd5bb
Az1zbekErgashev <ergasevaziz0611@gmail.com>
2025-04-01 16:50:39 +0900

    fixed soem bugs and added new type

--- ManagementBot/Service/BotService.cs
+++ ManagementBot/Service/BotService.cs
@@ -307,7 +307,21 @@ namespace ManagementBot.Service
                     using (var scope = _scopeFactory.CreateScope())
                     {
                         var dbContext = scope.ServiceProvider.GetRequiredService<CrmDBContext>();
-                        var botRequest = new CrmRequest { ContactNumber = request.ContactNumber, RequestStatusId = 4, Date = DateTime.UtcNow.ToString("dd.MM.yyyy"), Deadline = DateTime.UtcNow.AddDays(3),  ResponsiblePerson = request.ResponsiblePerson, AdditionalInformation = request.AdditionalInformation, CompanyName = request.CompanyName, Department = request.Department, Email = request.Email, InquirySource = request.InquirySource, Notes = request.Notes, ProjectBudget = request.ProjectBudget, Priority = ProjectManagement.Domain.Enum.Priority.Normal, Status = ProjectManagement.Domain.Enum.ProjectStatus.ToDO, ChatId = chatId};
+                        var botRequest = new CrmRequest { ContactNumber = request.ContactNumber,
+                            RequestStatusId = 4,
+                            Date = DateTime.UtcNow.ToString("dd.MM.yyyy"),
+                            Deadline = DateTime.UtcNow.AddDays(3),
+                            ResponsiblePerson = request.ResponsiblePerson,
+                            AdditionalInformation = request.AdditionalInformation,
+                            ClientCompany = request.CompanyName,
+                            Department = request.Department,
+                            Email = request.Email,
+                            InquirySource = request.InquirySource,
+                            Notes = request.Notes, 
+                            ProjectBudget = request.ProjectBudget,
+                            Priority = ProjectManagement.Domain.Enum.Priority.Normal, 
+                            Status = ProjectManagement.Domain.Enum.ProjectStatus.ToDO, 
+                            ChatId = chatId};
                         dbContext.Requests.Add(botRequest);
                         await dbContext.SaveChangesAsync();
                     }
